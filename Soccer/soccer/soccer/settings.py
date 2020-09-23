3531bd3fa0dd5bbbab0cc6a7993229566c2e5b99
sebastian <sn0wek.maly@gmail.com>
2020-09-23 11:16:09 +0200

    Update settings.py

--- Soccer/soccer/soccer/settings.py
+++ Soccer/soccer/soccer/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'corsheaders.middleware.CordsMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
